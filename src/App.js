00e43b30a1267b168d1490f19aff7927d229daf6
Fatih Kabay <mfthkabay@gmail.com>
2024-03-08 20:52:25 +0300

    parallax website transfered to a plane

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="content">
+          <header className="header-main">
+            <div className="layers">
+              <div className="layer-head">
+                <div className="caption">Welcome to Parallax Website</div>
+                <div className="title">Dark Forest</div>
+              </div>
+            </div>
+          </header>
+        </div>
+      </div>
+    </>
   );
 }
 
