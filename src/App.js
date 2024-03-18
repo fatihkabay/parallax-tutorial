66d022b19800e59caa236057127a9d9dceb51db6
Fatih Kabay <mfthkabay@gmail.com>
2024-03-18 23:03:46 +0300

    article

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import layerBaseImage from "./img/layer-base.png";
 import layerFrontImage from "./img/layer-front.png";
 import layerMiddleImage from "./img/layer-middle.png";
+import dungeonImage from "./img/dungeon.jpg";
 
 function App() {
   return (
@@ -13,11 +14,29 @@ function App() {
                 <div className="caption">Welcome to Parallax Website</div>
                 <div className="title">Dark Forest</div>
               </div>
-              <div className="img-layer layer-base" style={{backgroundImage: `url(${layerBaseImage})`}}></div>
-              <div className="img-layer layer-mid" style={{backgroundImage: `url(${layerMiddleImage})`}}></div>
-              <div className="img-layer layer-front" style={{backgroundImage: `url(${layerFrontImage})`}}></div>
+              <div
+                className="img-layer layer-base"
+                style={{ backgroundImage: `url(${layerBaseImage})` }}
+              ></div>
+              <div
+                className="img-layer layer-mid"
+                style={{ backgroundImage: `url(${layerMiddleImage})` }}
+              ></div>
+              <div
+                className="img-layer layer-front"
+                style={{ backgroundImage: `url(${layerFrontImage})` }}
+              ></div>
             </div>
           </header>
+          <article
+            className="article-main"
+            style={{ backgroundImage: `url(${dungeonImage})` }}
+          >
+            <div className="m-article-content">
+              <h2 className="m-header">Story to be continued!</h2>
+              <p className="m-paragraph">...</p>
+            </div>
+          </article>
         </div>
       </div>
     </>
